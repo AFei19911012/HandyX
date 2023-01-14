8b79a1d7100ab7231b64a95d4b6156c8427a3b71
ff_wang <1105936347@qq.com>
2023-01-14 21:29:39 +0800

    :tada:

---
+++ GeometryX/Model/DataModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Media;
+
+namespace GeometryX.Model
+{
+    ///
+    /// ----------------------------------------------------------------
+    /// Copyright @CoderMan/CoderMan1012 2023 All rights reserved
+    /// Author      : CoderMan/CoderMan1012
+    /// Created Time: 2023/1/14 17:53:33
+    /// Description :
+    /// ------------------------------------------------------
+    /// Version      Modified Time              Modified By                               Modified Content
+    /// V1.0.0.0     2023/1/14 17:53:33    CoderMan/CoderMan1012                 
+    ///
+    public class DataModel
+    {
+        public string BrushStr { get; set; }
+
+        public Brush BrushShow { get; set; }
+
+        public string BrushName { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # HandyX
-WPF 项目
+WPF 小项目，学习自用
 
 1. Gif 录屏工具
+
 2. 根据 iconfont svg 代码生成 Geometry 资源图标
+
+   [![pSQSo0U.png](https://s1.ax1x.com/2023/01/14/pSQSo0U.png)](https://imgse.com/i/pSQSo0U)
+
+   [![pSQSImT.png](https://s1.ax1x.com/2023/01/14/pSQSImT.png)](https://imgse.com/i/pSQSImT)
+
 3. 
